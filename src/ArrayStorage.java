d3f67d42c16e96c684e65472c9e885398bd537a8
erolatex <rf@sudouser.ru>
2019-04-05 15:49:52 +0300

    ArrayStorage methods realization

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,51 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private Resume[] storage = new Resume[10000];
+    private int resumesCounter = 0;
 
     void clear() {
+        Arrays.fill(storage, null);
+        resumesCounter = 0;
     }
 
     void save(Resume r) {
+        storage[resumesCounter] = r;
+        resumesCounter++;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < resumesCounter; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < resumesCounter; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i] = storage[resumesCounter - 1];
+                storage[resumesCounter - 1] = null;
+                resumesCounter--;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] trimmedResumes = new Resume[resumesCounter];
+        System.arraycopy(storage, 0, trimmedResumes, 0, resumesCounter);
+        return trimmedResumes;
     }
 
     int size() {
-        return 0;
+        return resumesCounter;
     }
 }
